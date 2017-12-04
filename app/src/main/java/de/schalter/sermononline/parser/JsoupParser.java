62875a3f9a30d3121b84f4458f55b7de86ae9747
SchalterDev <schalter.dev@gmail.com>
2017-12-04 22:41:03 +0100

    Changed structure of jsoupparsers

---
+++ app/src/main/java/de/schalter/sermononline/parser/JsoupParser.java
@@ -0,0 +1,56 @@
+package de.schalter.sermononline.parser;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import de.schalter.sermononline.objects.SermonElement;
+
+/**
+ * Created by martin on 30.11.17.
+ */
+
+public abstract class JsoupParser {
+
+    protected String html;
+    protected String url;
+
+    /**
+     * Connect to the given url and download the html
+     * @param urlString url as String
+     * @throws IOException when the url is not an URL
+     */
+    public void connect(String urlString) throws IOException {
+        this.url = urlString;
+
+        URL url = new URL(urlString);
+        HttpURLConnection ucon = (HttpURLConnection) url.openConnection();
+
+        ucon.setRequestProperty("User-Agent", "Mozilla/5.0...");
+
+        InputStream is = ucon.getInputStream();
+        BufferedInputStream bis = new BufferedInputStream(is);
+
+        /*
+         * Read bytes to the Buffer until there is nothing more to read(-1).
+         */
+        byte[] contents = new byte[1024];
+
+        int bytesRead;
+        StringBuilder stringBuilder = new StringBuilder();
+        while( (bytesRead = bis.read(contents)) != -1){
+            stringBuilder.append(new String(contents, 0, bytesRead));
+        }
+
+        html = stringBuilder.toString();
+    }
+
+    /**
+     * parse the downloaded html file
+     * @throws NoDataFoundException when the downloaded html is empty (or not downloaded) or there are no results
+     */
+    public abstract void parse() throws NoDataFoundException;
+
+}
--- app/src/main/java/de/schalter/sermononline/parser/JsoupResultParser.java
+++ app/src/main/java/de/schalter/sermononline/parser/JsoupResultParser.java
@@ -11,56 +11,21 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import de.schalter.sermononline.objects.SermonListElement;
 
-public class JsoupResultParser {
+public class JsoupResultParser extends JsoupParser {
 
     private final int DATA_TABLE_COUNT = 2;
     private final int HEADERS_TD_COUNT = 1;
     private final int DATA_TDS_BEGINN = 2;
 
-    private String html;
-
     private List<String> headers;
     private List<SermonListElement> content;
 
-    /**
-     * connects to the given url and downloads the html
-     * @param urlString url as string
-     * @throws IOException when the url is not an URL
-     */
-    public void connect(String urlString) throws IOException {
-        URL url = new URL(urlString);
-        HttpURLConnection ucon = (HttpURLConnection) url.openConnection();
-
-        ucon.setRequestProperty("User-Agent", "Mozilla/5.0...");
-
-        InputStream is = ucon.getInputStream();
-        BufferedInputStream bis = new BufferedInputStream(is);
-        /*
-         * Read bytes to the Buffer until there is nothing more to read(-1).
-         */
-        byte[] contents = new byte[1024];
-
-        int bytesRead;
-        html = "";
-        while( (bytesRead = bis.read(contents)) != -1){
-            html += new String(contents, 0, bytesRead);
-        }
-    }
-
-    /**
-     * parse the downloaded html file
-     * @throws NoDataFoundException when the downloaded html is empty (or not downloaded) or there are no results
-     */
+    @Override
     public void parse() throws NoDataFoundException {
         if(html == null || html.equals(""))
             throw new NoDataFoundException();
