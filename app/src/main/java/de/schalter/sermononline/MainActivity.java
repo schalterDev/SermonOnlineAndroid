8dc49c41f336be3ea4fa174dc85c8cb12d07ec30
Martin Schalter <schalter.dev@gmail.com>
2018-10-06 19:04:07 +0200

    Added ads again

--- app/src/main/java/de/schalter/sermononline/MainActivity.java
+++ app/src/main/java/de/schalter/sermononline/MainActivity.java
@@ -126,11 +126,10 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        /*
         if (id == R.id.action_settings) {
             this.startActivity(new Intent(this, SettingsActivity.class));
             return true;
-        }*/
+        }
 
         if (id == R.id.action_privacy) {
             // open privacy website
